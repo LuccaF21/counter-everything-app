c5dba5e8ef60d4f906024b818c6d4a5ad1e2e061
Lucca Ferrari <68200120+LuccaF21@users.noreply.github.com>
2022-12-11 20:05:32 -0300

    Added Total Entries function

--- jscode.js
+++ jscode.js
@@ -1,7 +1,9 @@
 
 let saveEl = document.getElementById("save-el")
 let countEl = document.getElementById("count-el")
+let totalEl = document.getElementById("total-el")
 let count = 0
+let total = 0
 
 function increment() {
     count += 1
@@ -9,14 +11,17 @@ function increment() {
 }
 
 function save() {
+    total += 1
     let countStr = count + " - "
     saveEl.textContent += countStr
     countEl.textContent = 0
+    totalEl.textContent = total
     count = 0
 }
 
 function clearall() {
-    saveEl.textContent = ""
+    saveEl.textContent = "- "
     countEl.textContent = 0
+    totalEl.textContent = 0
     count = 0
 }
